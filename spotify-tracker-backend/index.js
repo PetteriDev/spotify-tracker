8f7ec60900d439d26e5159b1f9d02cb1aae11dfc
Your Name <you@example.com>
2023-08-23 17:25:52 +0300

    1

--- spotify-tracker-backend/index.js
+++ spotify-tracker-backend/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const path = require('path');
+const openurl = require('openurl');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
-
-// Serve Angular build files from the 'dist' directory
-app.use(express.static(path.join(__dirname, 'dist')));
-
 // Define the artist schema and model
 const artistSchema = new mongoose.Schema({
   name: String,
@@ -33,19 +33,14 @@ mongoose.connect('mongodb://127.0.0.1:27017/spotify', { useNewUrlParser: true, u
     console.error('Failed to connect to MongoDB:', error);
   });
 
-app.get('/api/artists', (req, res) => {
-  Artist.find({})
-    .then(artists => {
-      res.json(artists);
-    })
-    .catch(err => {
-      console.error('Failed to retrieve artists:', err);
-      res.status(500).json({ error: 'Failed to retrieve artists' });
-    });
-});
-
-// Redirect all other requests to the Angular app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'angular/dist/index.html'));
-});
+  app.get('/api/artists', (req, res) => {
+    Artist.find({})
+      .then(artists => {
+        res.json(artists);
+      })
+      .catch(err => {
+        console.error('Failed to retrieve artists:', err);
+        res.status(500).json({ error: 'Failed to retrieve artists' });
+      });
+  });
   
